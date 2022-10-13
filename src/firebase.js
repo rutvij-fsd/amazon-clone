3aad5780d698c4bc1767b2dc04b5c77424f64233
rutvij-fsd <rutvijpatel414@gmail.com>
2022-10-14 00:26:47 +0530

    Setup Ready

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5rFfmOHaOazB0KxVA_HUb7Qo68J6Krjw",
+  authDomain: "clone-a3b95.firebaseapp.com",
+  projectId: "clone-a3b95",
+  storageBucket: "clone-a3b95.appspot.com",
+  messagingSenderId: "1054824639594",
+  appId: "1:1054824639594:web:dc1a34ce2551e5724cae13",
+};
